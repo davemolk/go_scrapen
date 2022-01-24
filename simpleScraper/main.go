7bff8d2f8bc3da54e6339d84beef6ad83c3ce84d
davemolk <davemolk@gmail.com>
2022-01-24 09:42:16 -0700

    basic scraper for all links

---
+++ simpleScraper/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+// Find and return all links
+func main() {
+	
+	c := colly.NewCollector(
+		colly.AllowedDomains("en.wikipedia.org"),
+	)
+
+	c.OnHTML(".mw-parser-output", func(e *colly.HTMLElement) {
+		links := e.ChildAttrs("a", "href")
+		fmt.Println(links)
+	})
+
+	c.Visit("https://en.wikipedia.org/wiki/Go_(programming_language)")
+
+}
